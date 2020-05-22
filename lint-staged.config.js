fcf2771d3cd901569db406016002385ce6238380
Oliver Ross <oliver.ross01@bbc.co.uk>
2020-05-22 22:00:07 +0100

    chore: use prettier not standard

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-  'package.json': files => [
-    ...files.map(file => `sort-package-json ${file}`)
+  "package.json": (files) => [
+    ...files.map((file) => `sort-package-json ${file}`),
   ],
-  '*.js': files => [
-    ...files.map(file => `standard ${file} --env jest --fix`)
-  ],
-  '*.json': files => [
-    ...files.map(file => `jsonlint ${file} -si`)
-  ],
-  '*.*': files => [
-    ...files.map(file => `git add ${file}`)
-  ]
-}
+  "*.{js,ts}": (files) => `prettier --write ${files.join(" ")}`,
+  "*.json": (files) => [...files.map((file) => `jsonlint ${file} -si`)],
+  "*.*": (files) => [...files.map((file) => `git add ${file}`)],
+};
