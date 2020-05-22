df2b4a22a239712e3da851d8e9148e461c0898d6
Oliver Ross <oliver.ross01@bbc.co.uk>
2020-05-22 22:02:33 +0100

    test: test commit

--- packages/game/src/app.ts
+++ packages/game/src/app.ts
@@ -1,7 +1,13 @@
 export class Test {
+  private sha = 1;
+
   constructor(private _name: string) {}
 
   public sayHello() {
+    let sha = 1;
+
+    console.log(sha, this.sha);
+
     console.log(`hello ${this._name}`);
   }
 }
