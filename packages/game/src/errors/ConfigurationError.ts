ad177139204e833083dc45b2a47ae84a337e6260
Oliver Ross <oliver.ross01@bbc.co.uk>
2020-05-23 13:56:25 +0100

    feat(game): add ConfigurationError

---
+++ packages/game/src/errors/ConfigurationError.ts
@@ -0,0 +1,7 @@
+export class ConfigurationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ConfigurationError";
+    Object.setPrototypeOf(this, ConfigurationError.prototype);
+  }
+}
