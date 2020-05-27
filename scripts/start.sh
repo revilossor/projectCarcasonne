d7313ab3f1469229fecfd745ebb43ec2e0718ade
Oliver Ross <oliver.ross01@bbc.co.uk>
2020-05-27 21:56:08 +0100

    feat: nice start script with cleanup

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+cleanup() {
+    npm --prefix packages/assets stop
+    docker system prune --force
+    exit 0
+}
+
+trap "cleanup" INT
+
 npm --prefix packages/assets start
 
 npm --prefix packages/game start
--- scripts/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-npm --prefix packages/assets stop
-
-docker system prune --force
